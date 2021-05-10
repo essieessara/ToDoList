11ce5b353fbd77fdd6b0143e6e4b576d6a089e3f
esraa ahmed <esraa.ahmed@bgomla.com>
2021-05-10 15:34:02 +0200

    remove files from blazor

---
+++ TodoList.Client/Services/HttpService.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+
+namespace BlazorApp.Services
+{
+
+    public class HttpService 
+    {
+
+    }
+}
